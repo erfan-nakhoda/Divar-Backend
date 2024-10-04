306ae5a64613fee327ea2ec1ca74be40da0b34d6
erfan8005 <erfinakhoda@gmail.com>
2024-10-04 13:24:32 +0330

    adding packages and starting app

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const express = require('express');
+require("dotenv").config();
 function main() {
-
+    const app = express();
+    app.use(express.json());
+    app.use(express.urlencoded({extended : true}));
+    app.listen(process.env.PORT, (err) => {
+        err ? console.log(err) : console.log(`http://localhost:${process.env.PORT}`);
+    });
 }
 main();
